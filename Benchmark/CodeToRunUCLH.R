b4d2299979ad26f2dca873bfd50ced893197c245
andysouth <andrew.south2@nhs.net>
2025-10-15 11:05:01 +0000

    add UCLH connection

--- Benchmark/CodeToRunUCLH.R
+++ Benchmark/CodeToRunUCLH.R
@@ -1,20 +1,33 @@
 # to restore renv environment
 renv::restore()
 
-# data base name, use an acronym to identify the database (e.g "CPRD GOLD")
-dbName <- "..."
+# omop_reservoir version
+# beware dbName identifies outputs, dbname is UCLH db
+dbName <- "UCLH-from-2019"
+cdmSchema <- "data_catalogue_007" #from 2019
+user <- Sys.getenv("user")
+host <- Sys.getenv("host")
+port <- Sys.getenv("port")
+dbname <- Sys.getenv("dbname")
+pwd <- Sys.getenv("pwd")
+writeSchema <- "_other_andsouth"
 
-# create a DBI connection to your database
-con <- DBI::dbConnect("...")
+if("" %in% c(user, host, port, dbname, pwd, writeSchema))
+  stop("seems you don't have (all?) db credentials stored in your .Renviron file, use usethis::edit_r_environ() to create")
 
-# schema in the database that contains OMOP CDM standard tables
-cdmSchema <- "..."
+#pwd <- rstudioapi::askForPassword("Password for omop_db")
 
-# schema in the database where you have writing permissions
-writeSchema <- "..."
+con <- DBI::dbConnect(RPostgres::Postgres(),user = user, host = host, port = port, dbname = dbname, password=pwd)
+
+#you get this if not connected to VPN
+#Error: could not translate host name ... to address: Unknown host
+
+#list tables
+DBI::dbListObjects(con, DBI::Id(schema = cdmSchema))
+DBI::dbListObjects(con, DBI::Id(schema = writeSchema))
 
 # created tables will start with this prefix
-prefix <- "..."
+prefix <- "heron_benchmark"
 
 # minimum cell counts used for suppression
 minCellCount <- 5
@@ -25,8 +38,10 @@ cdm <- CDMConnector::cdmFromCon(
   cdmSchema = cdmSchema,
   writeSchema =  writeSchema,
   writePrefix = prefix,
-  cdmName = dbName
+  cdmName = dbName#,
+  #.softValidation = TRUE
 )
+
 
 # run study code
 source("R/RunBenchmark.R")
